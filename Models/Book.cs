c21d73cf4ecdf52c695665be8ea632210a6e7ce2
attila <attila.istvan.toth@gmail.com>
2022-11-06 20:08:38 +0200

    Fix PublisherName in Books Index.cshtml
    + set Publisher nullable

--- Models/Book.cs
+++ Models/Book.cs
@@ -18,8 +18,8 @@ namespace Toth_Attila_Lab2.Models
         public DateTime PublishingDate { get; set; }
 
         public int PublisherID { get; set; }
-        public Publisher Publisher { get; set; } // navigation property
+        public Publisher? Publisher { get; set; } // navigation property
 
-        public ICollection<BookCategory> BookCategories { get; set; }
+        public ICollection<BookCategory>? BookCategories { get; set; }
     }
 }
