f3fcadf64e0ea2a5c7b0c01da10d629d17b6e332
attila <attila.istvan.toth@gmail.com>
2022-11-06 21:33:42 +0200

    Add PublisherName to Details page

--- Pages/Books/Details.cshtml.cs
+++ Pages/Books/Details.cshtml.cs
@@ -28,7 +28,7 @@ namespace Toth_Attila_Lab2.Pages.Books
                 return NotFound();
             }
 
-            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            Book? book = await _context.Book.Include(b => b.Publisher).FirstOrDefaultAsync(m => m.ID == id);
             if (book == null)
             {
                 return NotFound();
