25256f689b80780caf139f74247a79c1ed875e37
attila <attila.istvan.toth@gmail.com>
2022-11-14 00:02:28 +0200

    Add Author full name at delete

--- Pages/Books/Delete.cshtml.cs
+++ Pages/Books/Delete.cshtml.cs
@@ -29,7 +29,9 @@ namespace Toth_Attila_Lab2.Pages.Books
                 return NotFound();
             }
 
-            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            var book = await _context.Book
+                .Include(i => i.Author)
+                .FirstOrDefaultAsync(m => m.ID == id);
 
             if (book == null)
             {
