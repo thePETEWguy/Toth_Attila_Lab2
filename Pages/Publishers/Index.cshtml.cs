abd45abdebc9f5cf324e3f2e9c7458b99477393b
attila <attila.istvan.toth@gmail.com>
2022-12-18 14:30:36 +0200

    Lab4
    Done till (including) 13)

--- Pages/Publishers/Index.cshtml.cs
+++ Pages/Publishers/Index.cshtml.cs
@@ -7,25 +7,38 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Toth_Attila_Lab2.Data;
 using Toth_Attila_Lab2.Models;
+using Toth_Attila_Lab2.Models.ViewModels;
 
 namespace Toth_Attila_Lab2.Pages.Publishers
 {
     public class IndexModel : PageModel
     {
-        private readonly Toth_Attila_Lab2.Data.Toth_Attila_Lab2Context _context;
+        private readonly Toth_Attila_Lab2Context _context;
 
-        public IndexModel(Toth_Attila_Lab2.Data.Toth_Attila_Lab2Context context)
+        public IndexModel(Toth_Attila_Lab2Context context)
         {
             _context = context;
         }
 
         public IList<Publisher> Publisher { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public PublisherIndexData PublisherData { get; set; }
+        public int PublisherID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            if (_context.Publisher != null)
+            PublisherData = new PublisherIndexData();
+            PublisherData.Publishers = await _context.Publisher
+            .Include(i => i.Books)
+            .ThenInclude(c => c.Author)
+            .OrderBy(i => i.PublisherName)
+            .ToListAsync();
+            if (id != null)
             {
-                Publisher = await _context.Publisher.ToListAsync();
+                PublisherID = id.Value;
+                Publisher publisher = PublisherData.Publishers
+                .Where(i => i.ID == id.Value).Single();
+                PublisherData.Books = publisher.Books;
             }
         }
     }
