a0c5e46938e98da7ac319bd913b25bf5ced75463
attila <attila.istvan.toth@gmail.com>
2022-12-18 14:30:36 +0200

    Lab4

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,10 @@
+﻿using Toth_Attila_Lab2.Models;
+
+namespace Toth_Attila_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -12,9 +12,9 @@ namespace Toth_Attila_Lab2.Pages.Books
 {
     public class IndexModel : PageModel
     {
-        private readonly Toth_Attila_Lab2.Data.Toth_Attila_Lab2Context _context;
+        private readonly Toth_Attila_Lab2Context _context;
 
-        public IndexModel(Toth_Attila_Lab2.Data.Toth_Attila_Lab2Context context)
+        public IndexModel(Toth_Attila_Lab2Context context)
         {
             _context = context;
         }
@@ -23,8 +23,11 @@ namespace Toth_Attila_Lab2.Pages.Books
         public BookData BookD { get; set; }
         public int BookID { get; set; }
         public int CategoryID { get; set; }
+        public string TitleSort { get; set; }
+        public string AuthorSort { get; set; }
+        public string CurrentFilter { get; set; }
 
-        public async Task OnGetAsync(int? id, int? categoryID)
+        public async Task OnGetAsync(int? id, int? categoryID, string sortOrder, string searchString)
         {
             BookD = new BookData
             {
@@ -41,12 +44,37 @@ namespace Toth_Attila_Lab2.Pages.Books
                 .ToListAsync()
             };
 
+            TitleSort = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            AuthorSort = String.IsNullOrEmpty(sortOrder) ? "author_desc" : "";
+
+            CurrentFilter = searchString;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                BookD.Books = BookD.Books.Where(s => s.Author.FirstName.Contains(searchString)
+               || s.Author.LastName.Contains(searchString)
+               || s.Title.Contains(searchString));
+            }
+
+
             if (id != null)
             {
                 BookID = id.Value;
                 Book book = BookD.Books
                 .Where(i => i.ID == id.Value).Single();
                 BookD.Categories = book.BookCategories.Select(s => s.Category);
+            }
+
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    BookD.Books = BookD.Books.OrderByDescending(s =>
+                   s.Title);
+                    break;
+                case "author_desc":
+                    BookD.Books = BookD.Books.OrderByDescending(s =>
+                   s.Author.FullName);
+                    break;
             }
         }
     }
